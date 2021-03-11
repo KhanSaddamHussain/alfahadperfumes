73ae3d305811c0dcaece788eca57f0798cf70164
Saddam Khan <khansaddamhussain2016@gmail.com>
2021-03-11 09:46:10 +0530

    initial commit

---
+++ store/urls.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from django.urls import path
+from .views.home import Index , store
+from .views.signup import Signup
+from .views.login import Login , logout
+from .views.cart import Cart
+from .views.checkout import CheckOut
+from .views.orders import OrderView
+from .middlewares.auth import  auth_middleware
+
+#Django admin header customization
+admin.site.site_header = "Saddam khan"
+admin.site.site_title = " Welcome to Saddam's Dashboard"
+admin.site.index_title = "Welcome to admin  portal"
+
+
+
+urlpatterns = [
+    path('', Index.as_view(), name='homepage'),
+    path('store', store , name='store'),
+
+    path('signup', Signup.as_view(), name='signup'),
+    path('login', Login.as_view(), name='login'),
+    path('logout', logout , name='logout'),
+    path('cart', auth_middleware(Cart.as_view()) , name='cart'),
+    path('check-out', CheckOut.as_view() , name='checkout'),
+    path('orders', auth_middleware(OrderView.as_view()), name='orders'),
+]
